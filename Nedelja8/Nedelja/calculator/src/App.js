8c196ca4f9cea3cab674b24dd6bd0d5ce5ee2719
BogdanVulic <bogdanvulic92@gmail.com>
2021-01-30 12:28:45 +0100

    uradjen domaci

---
+++ Nedelja8/Nedelja/calculator/src/App.js
@@ -0,0 +1,17 @@
+import Calculator from './Calculator';
+import { useState } from 'react';
+
+const App = () => {
+
+    const [result, setResult] = useState(0);
+    const [steps, setSteps] = useState('');
+
+    return (
+        <div>
+            <Calculator setSteps={setSteps} setResult={setResult} result={result} />
+            <p>{steps}</p>
+        </div>
+    )
+}
+
+export default App
